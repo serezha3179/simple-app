bc1ab9e0523824a3d39edee718930f9e9569179a
serezha3179 <sergeiabramkin.79@gmail.com>
2025-06-01 18:36:46 +0300

    add folder public

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = 9000;
 
-app.use(express.static(__dirname + '/src'))
+app.use(express.static(__dirname + '/public'))
 
 app.get("/api/weather", function(req,res) {
     return res.json({"temperature": 30})
